b3c314880cf3af532ff4b82ec24fb2f138c9676b
lucranders <lucas.rand.br@gmail.com>
2021-08-14 23:23:17 -0300

    extract correction

--- 00.Descriptive/01.Filters/03.vif.sh
+++ 00.Descriptive/01.Filters/03.vif.sh
@@ -22,19 +22,25 @@ echo ${hwe_}
 
 input=/raid/genevol/vcf_1000G/phase3_20130502_grch38positions/ALL.chr"$chr"_GRCh38.genotypes.20170504.vcf.gz
 
-outputVif="$pathSave"Vif/Vif_NAfr_chr"$chr"_sample_"$sample_"_maf_"$maf_"_hwe_"$hwe_"_vif_"$vif_"
-outputVif2="$pathSave"Vif/Vif_Geuvadis_chr"$chr"_sample_"$sample_"_maf_"$maf_"_hwe_"$hwe_"_vif_"$vif_"
-outputVif3="$pathSave"Vif/Vif_Full_chr"$chr"_sample_"$sample_"_maf_"$maf_"_hwe_"$hwe_"_vif_"$vif_"
+outputVif="$pathSave"Vif/Vif_NAfr_chr"$chr"_maf_"$maf_"_hwe_"$hwe_"_vif_"$vif_"
+outputVif2="$pathSave"Vif/Vif_Geuvadis_chr"$chr"_maf_"$maf_"_hwe_"$hwe_"_vif_"$vif_"
+outputVif3="$pathSave"Vif/Vif_Full_chr"$chr"_maf_"$maf_"_hwe_"$hwe_"_vif_"$vif_"
 
 
 samp=/raid/genevol/users/lucas/heritability/02.GCTA/data/nAfr.txt
 samp2=/raid/genevol/users/lucas/heritability/02.GCTA/data/samples.txt
-file_=filter_sample_"$sample_"_maf_"$maf_"_hwe_"$hwe_"
+file_=filter_sample_NAfr_maf_"$maf_"_hwe_"$hwe_"
 fileprune="$pathSave"ListSnpFilters/"$file_".txt
+file2_=filter_sample_Geuvadis_maf_"$maf_"_hwe_"$hwe_"
+fileprune2="$pathSave"ListSnpFilters/"$file2_".txt
+file3_=filter_sample_Full_maf_"$maf_"_hwe_"$hwe_"
+fileprune3="$pathSave"ListSnpFilters/"$file3_".txt
+
+
 
 plink --vcf $input --indep 50 5 $vif_ --extract $fileprune --vcf-half-call missing --keep $samp --out $outputVif
-plink --vcf $input --indep 50 5 $vif_ --extract $fileprune --vcf-half-call missing --keep $samp2 --out $outputVif2
-plink --vcf $input --indep 50 5 $vif_ --extract $fileprune --vcf-half-call missing --out $outputVif3
+plink --vcf $input --indep 50 5 $vif_ --extract $fileprune2 --vcf-half-call missing --keep $samp2 --out $outputVif2
+plink --vcf $input --indep 50 5 $vif_ --extract $fileprune3 --vcf-half-call missing --out $outputVif3
 
 done
 done
