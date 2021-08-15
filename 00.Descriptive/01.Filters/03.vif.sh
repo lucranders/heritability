ef539a8fead71c016cefa1754ac748ec514f2997
lucranders <lucas.rand.br@gmail.com>
2021-08-14 23:16:42 -0300

    adding filters vif calculation

--- 00.Descriptive/01.Filters/03.vif.sh
+++ 00.Descriptive/01.Filters/03.vif.sh
@@ -8,28 +8,35 @@
 #PBS -j oe
 #PBS -o VifFilters.txt
 
-pathRead=/raid/genevol/vcf_1000G/phase3_20130502_grch38positions
-pathSave=/scratch/genevol/users/lucas/Vif/
+pathSave=/scratch/genevol/users/lucas/
 
 for chr in $(seq 1 22); do
 for vif_ in 1.11 2.5 5; do
+for maf_ in 0.05 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8; do
+for hwe_ in 0.001 0.005 0.01 0.02 0.03 0.04 0.05; do
+
 echo ${chr}
 echo ${vif_}
+echo ${maf_}
+echo ${hwe_}
 
 input=/raid/genevol/vcf_1000G/phase3_20130502_grch38positions/ALL.chr"$chr"_GRCh38.genotypes.20170504.vcf.gz
 
-outputVif="$pathSave"Vif_NAfr_chr"$chr"_vif_"$vif_"
-outputVif2="$pathSave"Vif_Geuvadis_chr"$chr"_vif_"$vif_"
-outputVif3="$pathSave"Vif_Full_chr"$chr"_vif_"$vif_"
+outputVif="$pathSave"Vif/Vif_NAfr_chr"$chr"_sample_"$sample_"_maf_"$maf_"_hwe_"$hwe_"_vif_"$vif_"
+outputVif2="$pathSave"Vif/Vif_Geuvadis_chr"$chr"_sample_"$sample_"_maf_"$maf_"_hwe_"$hwe_"_vif_"$vif_"
+outputVif3="$pathSave"Vif/Vif_Full_chr"$chr"_sample_"$sample_"_maf_"$maf_"_hwe_"$hwe_"_vif_"$vif_"
 
 
 samp=/raid/genevol/users/lucas/heritability/02.GCTA/data/nAfr.txt
 samp2=/raid/genevol/users/lucas/heritability/02.GCTA/data/samples.txt
+file_=filter_sample_"$sample_"_maf_"$maf_"_hwe_"$hwe_"
+fileprune="$pathSave"ListSnpFilters/"$file_".txt
 
+plink --vcf $input --indep 50 5 $vif_ --extract $fileprune --vcf-half-call missing --keep $samp --out $outputVif
+plink --vcf $input --indep 50 5 $vif_ --extract $fileprune --vcf-half-call missing --keep $samp2 --out $outputVif2
+plink --vcf $input --indep 50 5 $vif_ --extract $fileprune --vcf-half-call missing --out $outputVif3
 
-plink --vcf $input --indep 50 5 $vif_ --vcf-half-call missing --keep $samp --out $outputVif
-plink --vcf $input --indep 50 5 $vif_ --vcf-half-call missing --keep $samp2 --out $outputVif2
-plink --vcf $input --indep 50 5 $vif_ --vcf-half-call missing --out $outputVif3
-
+done
+done
 done
 done
