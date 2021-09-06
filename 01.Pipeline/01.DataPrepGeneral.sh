18db770963339daac10dc6365539de78fac02ca8
lucranders <lucas.rand.br@gmail.com>
2021-09-05 21:19:37 -0300

    make bed files

---
+++ 01.Pipeline/01.DataPrepGeneral.sh
@@ -0,0 +1,37 @@
+
+#!/bin/bash
+
+#PBS -l nodes=1:ppn=16
+#PBS -l mem=64gb
+#PBS -l walltime=24:00:00
+#PBS -q short
+#PBS -t 1-22
+#PBS -N bedMaker
+#PBS -j oe
+#PBS -o bedMaker.txt
+
+
+chr=$PBS_ARRAYID
+pop=$1
+maf=$2
+hwe=$3
+vif=$4
+
+pathRead=pathSave=/scratch/genevol/users/lucas/Vif/
+
+filteredSnps=Vif/Vif_"$pop"_chr"$chr"_maf_"$maf_"_hwe_"$hwe_"_vif_"$vif_".prune.in
+input=/raid/genevol/vcf_1000G/phase3_20130502_grch38positions/ALL.chr"$chr"_GRCh38.genotypes.20170504.vcf.gz
+bed=/scratch/genevol/users/lucas/TempBed/chr"$chr"
+
+
+if [ $1 = NAfr ]; then
+    filesamp=/raid/genevol/users/lucas/heritability/02.GCTA/data/nAfr.txt
+    plink --vcf $input --vcf-half-call missing --keep $filesamp --extract $filteredSnps --make-bed --out $pruneddata
+elif [ $2 = Geuvadis ]; then
+    filesamp=/raid/genevol/users/lucas/heritability/02.GCTA/data/samples.txt
+    plink --vcf $input --vcf-half-call missing --keep $filesamp --extract $filteredSnps --make-bed --out $pruneddata
+else
+    plink --vcf $input --vcf-half-call missing --extract $filteredSnps --make-bed --out $pruneddata
+fi
+
+
