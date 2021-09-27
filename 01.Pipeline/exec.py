d90b1b90969e4fbeaa26d71afbb2c81d92974913
lucranders <lucas.rand.br@gmail.com>
2021-09-26 23:39:15 -0300

    indent corrections + mkdir before dataprep

--- 01.Pipeline/exec.py
+++ 01.Pipeline/exec.py
@@ -30,7 +30,6 @@ def updateLog(path_,status_):
 
 # Create .bed files
 def createBedFile(pop_, maf_ , hwe_ , vif_):
-
     cmd = ['qsub', '-v' ,'pop=' + pop_ + ',maf_=' + str(maf_) +',hwe_=' + str(hwe_) + ',vif_=' + str(vif_) ,'/raid/genevol/users/lucas/heritability/01.Pipeline/01.DataPrepGeneral.sh']
     subprocess.Popen(cmd)
 
@@ -38,27 +37,25 @@ def calculateGCTA(path_,pop_,threads_):
     
     if pop_ == 'NAfr':
         filesamp='/raid/genevol/users/lucas/heritability/01.Pipeline/Samples/nAfr.filt'
-        cmd = ['/raid/genevol/users/lucas/gcta/gcta64', '--mbfile' ,path_ + '/chrs.txt','--keep',filesamp,'--make-grm','--out','GCTA','--thread-num',threads_]
+        cmd = ['/raid/genevol/users/lucas/gcta/gcta64', '--mbfile' ,path_ + '/chrs.txt','--keep',filesamp,'--make-grm','--out',path_+'/GCTA','--thread-num',threads_]
     elif pop_ == 'Geuvadis':
         filesamp='/raid/genevol/users/lucas/heritability/01.Pipeline/Samples/samples.filt'
-        cmd = ['/raid/genevol/users/lucas/gcta/gcta64', '--mbfile' ,path_ + '/chrs.txt','--keep',filesamp,'--make-grm','--out','GCTA','--thread-num',threads_]
+        cmd = ['/raid/genevol/users/lucas/gcta/gcta64', '--mbfile' ,path_ + '/chrs.txt','--keep',filesamp,'--make-grm','--out',path_+'/GCTA','--thread-num',threads_]
     else:
-        cmd = ['/raid/genevol/users/lucas/gcta/gcta64', '--mbfile' ,path_ + '/chrs.txt','--make-grm','--out','GCTA','--thread-num',threads_]
+        cmd = ['/raid/genevol/users/lucas/gcta/gcta64', '--mbfile' ,path_ + '/chrs.txt','--make-grm','--out',path_+'/GCTA','--thread-num',threads_]
     
     subprocess.Popen(cmd)
 
 
 # Create GRM, after creation of .bed file
 def createGRM(pop_, maf_ , hwe_ , vif_ , threads_):
-    
+    path_ = '/scratch/genevol/users/lucas/TempBed_pop_' + pop_ + "_maf_" + str(maf_) + "_hwe_" + str(hwe_) + "_vif_" + str(vif_)
+    subprocess.Popen(['mkdir',path_])
     # Create temp folder and .bed files
     createBedFile(pop_, maf_ , hwe_ , vif_)
-    path_ = '/scratch/genevol/users/lucas/TempBed_pop_' + pop_ + "_maf_" + str(maf_) + "_hwe_" + str(hwe_) + "_vif_" + str(vif_)
-
     # Compute amount of chromossomes processed
     sizes = checkLogSizes(path_)
     cond = sum(sizes) < 22
-
     # Wait until createBedFile is processed
     while cond:
         sizes = checkLogSizes(path_)
@@ -68,20 +65,18 @@ def createGRM(pop_, maf_ , hwe_ , vif_ , threads_):
             time.sleep(10)
         else:
             updateLog(path_,1)
-
     for chr_ in range(1,23):
         f = open(path_ + '/chrs.txt', "a")
-        f.write('chr' + str(chr_) + '\n')
+        f.write(path_+'/chr' + str(chr_) + '\n')
         f.close()
-    
     calculateGCTA(path_,pop_,str(threads_))
 
 
 
 
-pop = 'NAfr'
+pop = 'Geuvadis'
 maf = 0.01
-hwe = 1e-08
+hwe = 1e-07
 vif = 5
 threads = 10
 
