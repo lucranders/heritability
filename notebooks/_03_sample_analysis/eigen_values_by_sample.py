d4499f8b52be8b6a76f719d24c42748f32500e41
lucranders <lucas.rand.br@gmail.com>
2023-04-01 23:47:46 +0000

    ended study about eigen values (given different samples and configs)

--- notebooks/_03_sample_analysis/eigen_values_by_sample.py
+++ notebooks/_03_sample_analysis/eigen_values_by_sample.py
@@ -3,6 +3,14 @@ import itertools
 import os
 import numpy as np
 import pandas as pd
+from kedro.config import ConfigLoader
+from plotnine import *
+
+conf_paths = ["../../conf/local"]
+conf_loader = ConfigLoader(conf_paths)
+parameters = conf_loader.get("paths*", "paths*/**")
+temp_folder = parameters['pathTemp']
+
 
 mafs = [0.05,.1]
 # mafs = [0.01]
@@ -15,16 +23,16 @@ sexs = ['null']
 labs = ['null']
 genes = [
         ["HLA-A", "HLA-B", "HLA-C", "HLA-DQA1", "HLA-DPB1", "HLA-DRB1", "HLA-DPA1", "HLA-DQB1", "HLA-DRA"]
+        , ['HLA-A']
         ]
 
 outliers = [0.01, 'null']
-formulas = [{'fixed': '~pop + lab', 'random': "null"}]
 matrices_ = {'K_C':[-1]}
 sets_ = {'pipelineFullChrSer':[x for x in range(1,23)]}
 
 
-configs_ = []
-for element in itertools.product(mafs , pops , vifs , hwes , sexs , labs , outliers,genes,formulas):
+dfs_ = []
+for element in itertools.product(mafs , pops , vifs , hwes , sexs , labs , outliers,genes):
     maf_ = str(element[0])
     pop_ = element[1]
     vif_ = str(element[2])
@@ -33,87 +41,41 @@ for element in itertools.product(mafs , pops , vifs , hwes , sexs , labs , outli
     lab_ = element[5]
     outliers_ = str(element[6])
     genes_ = element[7]
-    formula_ = element[8]
     popStr_ = "_".join(pop_)
     sexStr_ = "_".join(sex_) if sex_ != 'null' else sex_
     labStr_ = "_".join(lab_) if lab_ != 'null' else lab_
     geneStr_ = "_".join(genes_)
     folder_name_ = f'''snpsParams_maf_{maf_}_hwe_{hwe_}_vif_{vif_}_sampParams_{popStr_}_sex_{sexStr_}_lab_{labStr_}_outliers_{outliers_}_genes_{geneStr_}'''
-    configs_.append([
-        maf_
-        , vif_
-        , hwe_
-        , outliers_
-        , popStr_
-        , folder_name_
-    ])
-
-configs_df_ = pd.DataFrame(configs_, columns = ['maf','vif','hwe','outliers','pop', 'folder'])
-
-def generateData(root_):
-    dfs_ = []
-    for file_ in os.listdir(root_):
+    for file_ in os.listdir(f'''{temp_folder}/{folder_name_}'''):
         if '.pkl' in file_:
-            matrix_ = pickle.load(open(f'{root_}/{file_}','rb'))
-            nameFile = file_.split('raw_')[1]
-            qtSnps = int(nameFile.replace('.pkl',''))
-            nameFile = file_.split('config_')[1]
-            configNumber_ = nameFile[0]
-            w,v = np.linalg.eig(matrix_)
-            df_ = pd.DataFrame(w,columns=['eigen'])
-            df_.loc[:, 'qtSnps'] = qtSnps
-            config_ = configs_[f'config_{configNumber_}']
-            df_.loc[:, 'configNumber'] = configNumber_
-            df_.loc[:, 'fileName'] = file_
-            df_.loc[:,'vif'] = config_['vif']
-            df_.loc[:,'maf'] = config_['maf']
-            df_.loc[:,'hwe'] = config_['hwe']
-            df_.loc[:,'config'] = 'hwe:' + df_.hwe.astype(str) + ' maf:' + df_.maf.astype(str) + ' vif:' + df_.vif.astype(str) + ' snps:' + df_.qtSnps.astype(str)
-            if df_.eigen.min() < 0:
-                df_.loc[:,'negative'] = 1
-            else:
-                df_.loc[:,'negative'] = 0
-            dfs_.append(df_)
-    finalDf_ = pd.concat(dfs_, axis = 0, ignore_index = True)
-    return finalDf_    
+            if 'correction_non_negative' not in file_:
+                print(genes_, len(genes_))
+                matrix_ = pickle.load(open(f'{temp_folder}/{folder_name_}/{file_}','rb'))
+                w,v = np.linalg.eig(matrix_)
+                df_ = pd.DataFrame(w,columns=['eigen'])
+                df_.loc[:, 'fileName'] = file_
+                df_.loc[:,'vif'] = vif_
+                df_.loc[:,'maf'] = maf_
+                df_.loc[:,'hwe'] = hwe_
+                df_.loc[:,'outliers'] = outliers_
+                df_.loc[:,'pop'] = 'European' if len(pop_) == 4 else 'All'
+                df_.loc[:,'genes'] = 'Solo' if len(genes_) == 1 else 'All'
+                df_.loc[:, 'n_sample'] = df_.shape[0]
+                if df_.eigen.min() < 0:
+                    df_.loc[:,'status'] = 'negative'
+                else:
+                    df_.loc[:,'status'] = 'positive'
+                dfs_.append(df_)
 
-def bxPltKC(root_, ax_, title_):
-    dfBxPlt = generateData(root_)
-    sns.boxplot(data = dfBxPlt, y = 'eigen', x = 'config', ax = ax_, )
-    ax_.tick_params(labelrotation=90)
-    ax_.set_title(title_)
-    # medians = dfBxPlt.groupby(['config'])['eigen'].median().values
-    numberSnps = dfBxPlt.groupby(['config'])['qtSnps'].max().values.astype(str)
-    negativeFlag = dfBxPlt.groupby(['config'])['negative'].max().values
-    mins_ = dfBxPlt.groupby(['config'])['eigen'].min().values
-    numberSnps = ["snps: " + i for i in numberSnps]
-    pos = range(len(numberSnps))
-    for tick,label in zip(pos,ax_.get_xticklabels()):
-        # ax_.text(pos[tick],
-        #         medians[tick] + .03,
-        #         numberSnps[tick],
-        #         horizontalalignment='center',
-        #         size='x-small',
-        #         color='red',
-        #         rotation = 'vertical',
-        #         weight='semibold')
-        if negativeFlag[tick] == 1:
-            ax_.text(pos[tick],
-                    mins_[tick] + 0.03,
-                    np.round(mins_[tick],7),
-                    horizontalalignment='center',
-                    size='x-small',
-                    color='red',
-                    weight='semibold')
-    return ax_
-    
-fig, (ax0,ax1,ax2,ax3) = plt.subplots(1, 4,  sharex = False,figsize=(16,16))
-bxPltKC('/scratch/genevol/users/lucas/sample1/', ax0, 'Everyone')
-bxPltKC('/scratch/genevol/users/lucas/sample2/', ax1, 'Everyone but outliers')
-bxPltKC('/scratch/genevol/users/lucas/sample3/', ax2, 'Europeans')
-bxPltKC('/scratch/genevol/users/lucas/sample4/', ax3, 'Europeans but outliers')
-# * 1 - Everyone, no outlier filters;
-# * 2 - Everyone, filtering out outliers based on Mahalanobis distance (percentile 99%);
-# * 3 - Europeans, no outlier filters;
-# * 4 - Europeans, filtering out outliers based on Mahalanobis distance (percentile 99%)
-fig.tight_layout()
+
+finalDf_ = pd.concat(dfs_, axis = 0, ignore_index = True)
+finalDf_.sort_values(['vif','maf','hwe','outliers'], inplace = True)
+finalDf_.loc[:, 'combination_'] = finalDf_.apply(lambda x: f'''{x['vif']}_{x['maf']}_{x['hwe']}_{x['outliers']}''', axis = 1)
+
+p_ = ggplot(finalDf_, aes(x = 'combination_', y='eigen', colour = 'status')) +\
+    geom_boxplot() +\
+    facet_wrap(['genes','pop'], scales='free_y') +\
+    theme(figure_size=(12,9), axis_text_x=element_text(rotation=90, hjust=1))
+
+p_.save('boxplot_eigen_values')
+
